12685f702d0e71692c01e0226df79308e9091432
Trilok Chaudhary <trilokchy999@gmail.com>
2024-12-05 15:56:45 +0545

    project uploaded

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ColorPicker from "./components/ColorPicker"
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <ColorPicker />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ColorPicker.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import Copy from "../assets/copy.svg"
+
+const ColorPicker = () => {
+    const [color, setColor] = useState("#ffffff")
+
+    const handleColorClick = () => {
+        navigator.clipboard.writeText(color)
+    }
+
+    const handleColorChange = (e) => {
+        setColor(e.target.value)
+    }
+
+    return (
+        <div className='color-container'>
+            <h1>Color Picker</h1>
+            <div className='color-selected' onClick={handleColorClick} style={{ backgroundColor: color, color: color }}>
+                <img src={Copy} alt="copy" />
+                <p>Color Selected: <span> {color} </span></p>
+            </div>
+            <label htmlFor="color">Pick Color:
+                <input type="color" id="color" onChange={handleColorChange} value={color} />
+            </label>
+        </div>
+    )
+}
+
+export default ColorPicker
